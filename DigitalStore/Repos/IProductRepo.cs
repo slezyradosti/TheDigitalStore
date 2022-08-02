a28c8658459fdc5fa083e3407ef79c95337a312d
Oleg <rredzer000@gmail.com>
2022-08-02 22:57:25 +0300

    Added 'repositories' for multiple code

---
+++ DigitalStore/Repos/IProductRepo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DigitalStore.Models;
+
+namespace DigitalStore.Repos
+{
+    public interface IProductRepo : IRepo<Product>
+    {
+        List<Product> Search(string searchString);
+       // List<Product> GetAllPromotionalProducts();
+        List<Product> GetRelatedData();
+    }
+}
---
+++ DigitalStore/Repos/ProductRepo.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DigitalStore.EF;
+using DigitalStore.Models;
+using Microsoft.EntityFrameworkCore;
+using static Microsoft.EntityFrameworkCore.EF;
+
+namespace DigitalStore.Repos
+{
+    public class ProductRepo : BaseRepo<Product>, IProductRepo
+    {
+        public ProductRepo(DigitalStoreContext context) : base(context)
+        {
+        }
+
+        public override List<Product> GetAll()
+            => GetAll(x => x.ProductName, true).ToList();
+
+        public List<Product> Search(string searchString)
+            => Context.Products.Where(p => Functions.Like(p.ProductName, $"%{searchString}%")).ToList();
+
+        //public List<Product> GetPromotionalProducts()
+        //    => GetSome(x => x.isPromoted == true);
+
+        public List<Product> GetRelatedData()
+            => Context.Products.FromSqlInterpolated($"SELECT * FROM Product").Include(x => x.Category).ToList();
+    }
+}
