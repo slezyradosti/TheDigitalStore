5ab816dc3ace00590904133211c6b264cc04aa7a
Oleg <rredzer000@gmail.com>
2022-08-24 23:35:59 +0300

    a little designe update

---
+++ DigitalStore.WebUI/ViewComponents/CategoryViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using DigitalStore.Repos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace DigitalStore.WebUI.ViewComponents
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        private readonly ICategoryRepo _repo;
+
+        public CategoryViewComponent(ICategoryRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories = _repo.GetAll().AsQueryable().OrderBy(c => c.CategoryName);
+            if (categories != null)
+            {
+                return View("CategoryPartialView", categories);
+            }
+            return new ContentViewComponentResult("Unable to locate records.");
+        }
+    }
+}
