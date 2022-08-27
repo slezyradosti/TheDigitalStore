cf7112bb162e8df50ab83e857c6fab59b5a9bac5
Oleg <rredzer000@gmail.com>
2022-08-27 21:48:59 +0300

    now email purchase alerts are sent

--- DigitalStore/Repos/EmailOrderProcessor.cs
+++ DigitalStore/Repos/EmailOrderProcessor.cs
@@ -35,68 +35,15 @@ namespace DigitalStore.Models.NotForDB
 
             using (var client = new SmtpClient())
             {
-                await client.ConnectAsync(emailSettings.ServerName, 25, emailSettings.UseSsl);
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                await client.ConnectAsync(emailSettings.ServerName, emailSettings.ServerPort, emailSettings.UseSsl);
                 await client.AuthenticateAsync(emailSettings.Username, emailSettings.Password);
                 await client.SendAsync(emailMessage);
 
                 await client.DisconnectAsync(true);
             }
-            //using (var smtpClient = new SmtpClient())
-            //{
-            //    smtpClient.EnableSsl = emailSettings.UseSsl;
-            //    smtpClient.Host = emailSettings.ServerName;
-            //    smtpClient.Port = emailSettings.ServerPort;
-            //    smtpClient.UseDefaultCredentials = false;
-            //    smtpClient.Credentials =
-            //        new NetworkCredential(emailSettings.Username, emailSettings.Password);
-
-            //    if (emailSettings.WriteAsFile)
-            //    {
-            //        smtpClient.DeliveryMethod
-            //            = SmtpDeliveryMethod.SpecifiedPickupDirectory;
-            //        smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
-            //        smtpClient.EnableSsl = false;
-            //    }
-
-            //    StringBuilder body = new StringBuilder()
-            //        .AppendLine("New order processed")
-            //        .AppendLine("---")
-            //        .AppendLine("Products:");
-
-            //    foreach (var line in cart.Lines)
-            //    {
-            //        var subtotal = line.Product.ProductPrice * line.Quantity;
-            //        body.AppendFormat("{0} x {1} (total: {2:c}",
-            //            line.Quantity, line.Product.ProductName, subtotal);
-            //    }
-
-            //    body.AppendFormat("Total cost: {0:c}", cart.ComputeTotalValue())
-            //        .AppendLine("---")
-            //        .AppendLine("Shipping:")
-            //        .AppendLine(customer.FirstName)
-            //        .AppendLine(customer.MidName)
-            //        .AppendLine(customer.LastName ?? "")
-            //        .AppendLine(customer.PhoneNumber ?? "")
-            //        .AppendLine(customer.City.CityName)
-            //        //.AppendLine(customer.Country)
-            //        .AppendLine("---");
-            //        //.AppendFormat("Gift wrapping: {0}",
-            //        //    customer.GiftWrap ? "Yes" : "No");
-
-            //    MailMessage mailMessage = new MailMessage(
-            //                           emailSettings.MailFromAddress,	// From
-            //                           emailSettings.MailToAddress,		// To
-            //                           "The new order has been sent!",		// Thepe
-            //                           body.ToString()); 				// Body of letter
-
-            //    if (emailSettings.WriteAsFile)
-            //    {
-            //        mailMessage.BodyEncoding = Encoding.UTF8;
-            //    }
-
-            //    smtpClient.Send(mailMessage);
-            //}
         }
+
         public async Task SendPurchaseEmailAsync(Customer customer, string subject, Cart cart)
         {
             var emailMessage = new MimeMessage();
@@ -135,22 +82,14 @@ namespace DigitalStore.Models.NotForDB
                 Text = body.ToString()
             };
 
-            //using (var client = new SmtpClient())
-            //{
-            //    await client.ConnectAsync(emailSettings.ServerName, emailSettings.ServerPort, emailSettings.UseSsl);
-            //    await client.AuthenticateAsync(emailSettings.Username, emailSettings.Password);
-            //    await client.SendAsync(emailMessage);
-
-            //    await client.DisconnectAsync(true);
-            //}
             try
             {
                 using (var client = new SmtpClient())
                 {
                     client.ServerCertificateValidationCallback = (s, c, h, e) => true;
-                    client.Connect(emailSettings.ServerName, emailSettings.ServerPort, emailSettings.UseSsl);
-                    client.Authenticate(emailSettings.Username, emailSettings.Password);
-                    client.Send(emailMessage);
+                    await client.ConnectAsync(emailSettings.ServerName, emailSettings.ServerPort, emailSettings.UseSsl);
+                    await client.AuthenticateAsync(emailSettings.Username, emailSettings.Password);
+                    await client.SendAsync(emailMessage);
 
                     await client.DisconnectAsync(true);
                 }
@@ -186,26 +125,13 @@ namespace DigitalStore.Models.NotForDB
         }
     }
 
-    //public class EmailSettings
-    //{
-    //    public string MailToAddress = "orders@example.com";
-    //    public string MailFromAddress = "digitaltore@example.com";
-    //    public bool UseSsl = true;
-    //    public string Username = "MySmtpUsername";
-    //    public string Password = "MySmtpPassword";
-    //    public string ServerName = "smtp.example.com";
-    //    public int ServerPort = 587;
-    //    public bool WriteAsFile = true;
-    //    public string FileLocation = @"c:\digital_store_emails";
-    //}
-
     public class EmailSettings
     {
-        public string MailFromAddress = "digitalstore@myself.com";
+        public string MailFromAddress = "oleg.sergushin11@mail.ru";
         public bool UseSsl = true;
-        public string Username = "digitalstore@myself.com";
-        public string Password = "digitalstore1";
-        public string ServerName = "smtp.mail.com";
+        public string Username = "oleg.sergushin11@mail.ru";
+        public string Password = "ir6g2cxADaGv2bNcSRqF";
+        public string ServerName = "smtp.mail.ru";
         public int ServerPort = 465;
     }
 }
