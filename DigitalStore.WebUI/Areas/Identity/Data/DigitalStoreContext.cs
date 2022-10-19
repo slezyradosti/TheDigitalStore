9c65e8e5575dab5f4cb7f9ba5351333cfbc15afe
Life is squid <rredzer000@gmail.com>
2022-10-19 11:38:40 +0100

    some fixes
    delete second auto-created dbcontext

--- DigitalStore.WebUI/Areas/Identity/Data/DigitalStoreContext.cs
+++ DigitalStore.WebUI/Areas/Identity/Data/DigitalStoreContext.cs
@@ -1,21 +1,21 @@
-﻿using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿//using Microsoft.AspNetCore.Identity;
+//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
 
-namespace DigitalStore.EF;
+//namespace DigitalStore.EF;
 
-public class DigitalStoreContext : IdentityDbContext<IdentityUser>
-{
-    public DigitalStoreContext(DbContextOptions<DigitalStoreContext> options)
-        : base(options)
-    {
-    }
+//public class DigitalStoreContext : IdentityDbContext<IdentityUser>
+//{
+//    public DigitalStoreContext(DbContextOptions<DigitalStoreContext> options)
+//        : base(options)
+//    {
+//    }
 
-    protected override void OnModelCreating(ModelBuilder builder)
-    {
-        base.OnModelCreating(builder);
-        // Customize the ASP.NET Identity model and override the defaults if needed.
-        // For example, you can rename the ASP.NET Identity table names and more.
-        // Add your customizations after calling base.OnModelCreating(builder);
-    }
-}
+//    protected override void OnModelCreating(ModelBuilder builder)
+//    {
+//        base.OnModelCreating(builder);
+//        // Customize the ASP.NET Identity model and override the defaults if needed.
+//        // For example, you can rename the ASP.NET Identity table names and more.
+//        // Add your customizations after calling base.OnModelCreating(builder);
+//    }
+//}
